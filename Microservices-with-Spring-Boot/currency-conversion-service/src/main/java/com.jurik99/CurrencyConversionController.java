16f289631f2be3e762b65190633076987ac68039
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-05-08 11:25:56 +0200

    Finished Section 4, Lecture 77 (Invoking Currency Exchange Microservice from Currency Conversion Microservice)

--- Microservices-with-Spring-Boot/currency-conversion-service/src/main/java/com.jurik99/CurrencyConversionController.java
+++ Microservices-with-Spring-Boot/currency-conversion-service/src/main/java/com.jurik99/CurrencyConversionController.java
@@ -1,10 +1,15 @@
 package com.jurik99;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class CurrencyConversionController {
@@ -13,6 +18,19 @@ public class CurrencyConversionController {
 	public CurrencyConversionBean convertCurrency(@PathVariable final String from,
 	                                              @PathVariable final String to,
 	                                              @PathVariable final BigDecimal quantity) {
-		return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+
+		final Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		final ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+		final CurrencyConversionBean responseBody =
+				Optional.ofNullable(responseEntity.getBody()).orElseThrow(() -> new RuntimeException("No body in the response entity!"));
+
+		final BigDecimal conversionMultiple = responseBody.getConversionMultiple();
+		return new CurrencyConversionBean(responseBody.getId(), responseBody.getFrom(), responseBody.getTo(),
+		                                  conversionMultiple, quantity, quantity.multiply(conversionMultiple),
+		                                  responseBody.getPort());
 	}
 }
