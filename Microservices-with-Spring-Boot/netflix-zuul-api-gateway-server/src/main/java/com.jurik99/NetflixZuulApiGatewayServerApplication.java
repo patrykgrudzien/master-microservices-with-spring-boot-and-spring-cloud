2d7224e701f62a094b06a499e300e6eb8abe275b
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-05-09 10:48:32 +0200

    Finished Section 4, Lecture 88 (Setting up Zuul API Gateway)

---
+++ Microservices-with-Spring-Boot/netflix-zuul-api-gateway-server/src/main/java/com.jurik99/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,16 @@
+package com.jurik99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class);
+	}
+}
