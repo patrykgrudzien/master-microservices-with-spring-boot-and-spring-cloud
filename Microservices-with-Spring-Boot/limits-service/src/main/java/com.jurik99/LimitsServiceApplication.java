047c636668337729c9f1e6777ce4b0302587d02b
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-05-09 09:21:34 +0200

    Finished Section 4, Lecture 84 (Connecting Currency Exchange & Limits Microservices to Eureka)

--- Microservices-with-Spring-Boot/limits-service/src/main/java/com.jurik99/LimitsServiceApplication.java
+++ Microservices-with-Spring-Boot/limits-service/src/main/java/com.jurik99/LimitsServiceApplication.java
@@ -2,8 +2,10 @@ package com.jurik99;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LimitsServiceApplication {
 
     public static void main(final String[] args) {
