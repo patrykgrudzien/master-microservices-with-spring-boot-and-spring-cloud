ef9ea85fe7bf894cc107f8a1bb7efc178e2990ab
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-05-10 14:05:56 +0200

    Started Section 4, Lecture 103 (Fault Tolerance with Hystrix)

--- Microservices-with-Spring-Boot/limits-service/src/main/java/com.jurik99/LimitsConfigurationController.java
+++ Microservices-with-Spring-Boot/limits-service/src/main/java/com.jurik99/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jurik99.bean.LimitsConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -17,5 +18,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfigurations() {
         return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitsConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available!");
+    }
+
+    public LimitsConfiguration fallbackRetrieveConfiguration() {
+        return new LimitsConfiguration(999, 9);
     }
 }
